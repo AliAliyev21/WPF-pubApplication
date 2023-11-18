701a899dadc1868fa1e19b71394b29c3aa8835fb
GitHub Kullanıcı Adınız <GitHub E-Posta Adresini>
2023-11-18 15:38:24 +0400

    My App

---
+++ WpfAppPub3/ViewModels/ShowHistroyModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppPub3.Models;
+
+namespace WpfAppPub3.ViewModels
+{
+    public class ShowHistroyModels:BaseViewModels
+    {
+        private ObservableCollection<Beer> purchaseHistory;
+
+        public ObservableCollection<Beer> PurchaseHistory
+        {
+            get { return purchaseHistory; }
+            set { purchaseHistory = value; OnPropertyChanged(); }
+        }
+
+        public ShowHistroyModels()
+        {
+            PurchaseHistory = new ObservableCollection<Beer>();
+        }
+
+        public double CalculateTotal()
+        {
+            return PurchaseHistory.Sum(beer => beer.Total);
+        }
+    }
+}
